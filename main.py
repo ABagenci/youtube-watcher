e65ad5a66a12172088e1c99944ca2f5ac65f187c
ABagenci <ab.agenci@outlook.com>
2025-05-05 18:22:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-WEBHOOK_URL = "https://script.google.com/macros/s/TWÓJ_WEBHOOK/exec"  # ← Twój adres z GAS
-VIDEO_ID = "iFyeXRu-asE"  # ← ID testowego filmu YouTube
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbw4GoRYIsXBGxoWt7TTKkcPa_wwIq1EBMgWsVUHKfkNYnC3cuIGbY6WYb6UdP2uTI8H/exec"  # ← Twój adres z GAS
+VIDEO_ID = "1JUbMksBruI"  # ← ID testowego filmu YouTube
 
 print("🎯 Wysyłam video_id do Colaba przez webhook GAS...")
 requests.post(WEBHOOK_URL, json={"video_id": VIDEO_ID})
